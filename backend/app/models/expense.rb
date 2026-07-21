ee3458c050a55652b1f1097de52cecb99d09ba9d
Zerpington <sean.jatayna.mnl@gmail.com>
2026-07-21 14:26:08 +0800

    BONUS-001: Add validation for future dates in expenses; Add tests for date validations for expenses

--- backend/app/models/expense.rb
+++ backend/app/models/expense.rb
@@ -1,3 +1,12 @@
 class Expense < ApplicationRecord
   belongs_to :category
+  validate :date_cannot_be_in_the_future
+
+  private
+
+  def date_cannot_be_in_the_future
+    return if date.blank?
+
+    errors.add(:date, "cannot be in the future") if date > Date.today
+  end
 end
