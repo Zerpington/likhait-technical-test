662f5466b06a1023c3556b169a0b988cca03ebe0
Zerpington <sean.jatayna.mnl@gmail.com>
2026-07-21 15:00:02 +0800

    BONUS-001: Add frontend validation for datepicker and manual date input to prevent future date subbmissions; Add current date as max date for calendar

--- frontend/src/hooks/useExpenseForm.ts
+++ frontend/src/hooks/useExpenseForm.ts
@@ -49,6 +49,10 @@ export function useExpenseForm({ initialData, onSubmit }: UseExpenseFormProps) {
       newErrors.date = "Date is required";
     }
 
+    if (formData.date > formatDate(new Date())) {
+      newErrors.date = "Expense date cannot be in the future"
+    }
+
     setErrors(newErrors);
     return Object.keys(newErrors).length === 0;
   };
